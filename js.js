e1f5b76e0ae6a09394a7060a2383cd7e2efa16ae
M.Ameen <ameen55668@gmail.com>
2022-07-24 09:01:59 +0500

    after completing click event

--- js.js
+++ js.js
@@ -2,16 +2,18 @@ let save = document.getElementsByClassName('save')[0]
 let parent = document.getElementsByClassName('parent')[0]
 let note = document.getElementsByClassName('note')[0] 
 let title = document.getElementsByClassName('title')[0] 
+let card = document.getElementsByClassName('cardb')[0]
+let del = document.getElementsByClassName('delete')
 console.log(save);
 console.log(parent);
 console.log(note);
 console.log(title);
 var index=1
 save.addEventListener('click',function(){
-    if (title.value === null) {
-        alert('please insert title & Notes')
+    if (title.value.length === 0 || note.value.length === 0) {
+        alert('Please insert title & Notes');
     }else{
-    parent.innerHTML +=`<div class="card text-dark bg-light mx-2 my-1" style="max-width: 18rem;">
+        parent.innerHTML +=`<div class="card text-dark bg-light mx-2 my-1 cardb" style="max-width: 18rem;">
     <div class="card-header"><b>Notes${index++}</b></div>
     <div class="card-body">
     <h5 class="card-title">${title.value}</h5>
